c2bba21f593f3febe2bf7429983aec928cb468c4
Theonom <theonomproduction@gmail.com>
2024-11-02 14:51:39 +0700

    add action in data testing
    kurang backstep

--- Assets/Scripts/AI/AIAction.cs
+++ Assets/Scripts/AI/AIAction.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class AIAction : MonoBehaviour
 {
     public AI ai;
+    public DataAI dataAI;
+
     private Animator anim;
 
     // Start is called before the first frame update
@@ -36,5 +38,31 @@ public class AIAction : MonoBehaviour
     public void PlaySfx(string nameSfx)
     {
         AudioManager.instance.PlaySfx(nameSfx);
+    }
+
+    public void AddDataToDataTesting(string action)
+    {
+        Data newData = new Data
+        {
+            actionData = action,
+            hpComparisionData = dataAI.hpComparision,
+            hpNPCData = dataAI.hpNPC,
+            spNPCData = dataAI.spNPC,
+            playerAttackData = dataAI.playerAttack,
+            positionYData = dataAI.positionY
+        };
+
+        if (GameManager.Instance.round == 1)
+        {
+            dataAI.dataRound[0].AddData(newData);
+        }
+        if (GameManager.Instance.round == 2)
+        {
+            dataAI.dataRound[1].AddData(newData);
+        }
+        if (GameManager.Instance.round == 3)
+        {
+            dataAI.dataRound[2].AddData(newData);
+        }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,7 +6,7 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
 
-    [SerializeField] private int round;
+    public int round;
     [SerializeField] private int playerWinAmount;
     [SerializeField] private int aiWinAmount;
 
